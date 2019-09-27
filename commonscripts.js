cd04b60994a848f36665fd4bf1cbf74abaf4944f
DNVIC <37513362+DNVIC@users.noreply.github.com>
2019-09-27 09:07:56 -0400

    Update commonscripts.js

--- commonscripts.js
+++ commonscripts.js
@@ -18,3 +18,22 @@ for (i = 0; i < x.length; i++) {
     //console.log(location.href + " Button " + i + " href");
 }
 
+// toggle when clicked
+function dropdown() {
+    document.getElementById("theselector").classList.toggle("show");
+}
+
+// close window when clicking off think
+window.onclick = function(event) {
+  if (!event.target.matches('.thelink')) {
+
+    var dropdowns = document.getElementsByClassName("insideselector");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openList = dropdowns[i];
+      if (openList.classList.contains('show')) {
+        openList.classList.remove('show');
+      }
+    }
+  }
+}
