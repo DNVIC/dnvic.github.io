da1f28b52f0f1c23c2dab2f73dfb7e2eb7c48b88
BuildTools <unconfigured@null.spigotmc.org>
2019-09-26 22:22:28 -0400

    Added cool stuff
    buttons that can be added without changing any javascript

--- commonscripts.js
+++ commonscripts.js
@@ -1,3 +1,20 @@
-document.getElementByClassName("btn").addEventListener("click", function(event) {
-  location.href = event.target.id;
-});
+
+
+
+
+var x = document.getElementsByClassName("btn");
+
+
+var i;
+
+for (i = 0; i < x.length; i++) {
+    console.log(x[i].id);
+    var d = [];
+    d.push[x[i].id]
+    document.getElementById(x[i].id).addEventListener("click", function(event) {
+        console.log(event.target.id);
+        location.href = event.target.id;
+    });
+    //console.log(location.href + " Button " + i + " href");
+}
+
