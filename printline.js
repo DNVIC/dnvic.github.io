cf547c9e55dfc4b46562acce6de9f6c747278142
DNVIC <37513362+DNVIC@users.noreply.github.com>
2019-12-02 08:41:10 -0500

    Update printline.js

--- printline.js
+++ printline.js
@@ -1,6 +1,6 @@
 def printline(var text) {
   var par = document.createElement("p");
   var text2 = document.createTextNode(text);
-  par.appendChild(text);
+  par.appendChild(text2);
   document.body.appendChild(par);
 }
